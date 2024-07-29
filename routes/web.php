f4e85ff8a468abd3e83e893128f300b6b8b2363b
hodaifayahia <hodaifayahia16@gmail.com>
2024-07-29 08:59:55 +0100

    implement updating profile details

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::post('/profile/update-iamges', [ProfileController::class, 'UpdateImages'])
          ->name('profile.updateimages');
 //     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
  });
 
 require __DIR__.'/auth.php';
